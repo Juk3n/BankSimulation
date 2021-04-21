4f4f138a8590a493984b69f0f099799e072ddb02
Juk3n <adekenn3@gmail.com>
2021-04-21 20:06:24 +0200

    transactions surrounded with semaphores

--- InitScript.c
+++ InitScript.c
@@ -2,8 +2,23 @@
 #include <stddef.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+
+    int sem_set_id = semget(250, 1, IPC_CREAT | 0644);
+    if (sem_set_id == -1) {
+        printf("Cannot create semaphore\n");
+    }
+    printf("semsetid: %i\n", sem_set_id);
+
+    int rc = semctl(sem_set_id, 0, SETVAL, 1);
+    if (rc == -1) {
+        printf("Cannot set value to semaphore\n");
+    }
+
     int id = fork();
     switch(id) {
     case -1:
@@ -14,7 +29,7 @@ int main(int argc, char *argv[]) {
         execl("./zad1", "./zad1", "2", "20", NULL);
 	    break;
 	default: // parent
-        execl("./zad1", "./zad1", "1", "2", "100", "1", NULL);
+        execl("./zad1", "./zad1", "1", "2", "100", "5", NULL);
         wait(NULL);
 		break;
 	}
--- zad1.c
+++ zad1.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
 
 void readBankBalance(int *accounts, int accountNumber) {
     printf("Bank Balance (account %i) : %i\n", accountNumber, accounts[accountNumber]);
@@ -33,6 +34,13 @@ int main(int argc, char *argv[]) {
 
     accounts = attachMemoryBlock();
     for(int i = 0; i < iterations; i++) {
+        int sem_set_id = semget(250, 1, IPC_CREAT | 0644);
+        struct sembuf sem_op;
+        sem_op.sem_num = 0;
+        sem_op.sem_op = -1;
+        sem_op.sem_flg = 0;
+        semop(sem_set_id, &sem_op, 1);
+
         if(argc == 2) {
             readBankBalance(accounts, atoi(argv[1]));
         }
@@ -51,6 +59,11 @@ int main(int argc, char *argv[]) {
         else {
             printf("Bad number of arguments!");
         }
+
+        sem_op.sem_num = 0;
+        sem_op.sem_op = 1;
+        sem_op.sem_flg = 0;
+        semop(sem_set_id, &sem_op, 1);
     }
     detachMemoryBlock(accounts);
 }
