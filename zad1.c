4694c6d585cfd0a1afedf462bdf38312e07337a1
Juk3n <adekenn3@gmail.com>
2021-04-21 12:06:36 +0200

    basic functionality on 1 account

--- zad1.c
+++ zad1.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+void readAccountBalance(int *accountBalance) {
+    printf("Account Ballance: %i\n", *accountBalance);
+}
+
+void increaseAccountBalance(int *accountBalance, int amountOfMoney) {
+    *accountBalance += amountOfMoney;
+}
+
+void decreaseAccountBalance(int *accountBalance, int amountOfMoney) {
+    *accountBalance -= amountOfMoney;
+}
+
 int main(int argc, char *argv[]) {
-    printf("Hello!\n");
+    int accountBalance = 0;
+    readAccountBalance(&accountBalance);
+    increaseAccountBalance(&accountBalance, 100);
+    readAccountBalance(&accountBalance);
+    decreaseAccountBalance(&accountBalance, 100);
+    readAccountBalance(&accountBalance);
 }
