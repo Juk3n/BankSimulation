9258ed5551b670eac6c2b2f36b9e0fcce1d1275e
Juk3n <adekenn3@gmail.com>
2021-05-05 11:43:09 +0200

    added header guards

--- semaphoreHelper.h
+++ semaphoreHelper.h
@@ -1,6 +1,11 @@
+#ifndef SEMAPHORE_HELPER_H
+#define SEMAPHORE_HELPER_H
+
 struct sembuf semaphoreBuffer;
 int semaphoreSetId;
 
 void semaphoreInit();
 void semaphoreOpen();
 void semaphoreClose();
+
+#endif
--- sharedMemoryHelper.h
+++ sharedMemoryHelper.h
@@ -1,3 +1,8 @@
+#ifndef SEMAPHORE_HELPER_H
+#define SEMAPHORE_HELPER_H
+
 int* attachMemoryBlock();
 void detachMemoryBlock(int* dataPointer);
 void destroyMemoryBlock();
+
+#endif
